d51efb325e770a99f96bd895fbad01efa7ddd236
karthik <sowrya.m@cumulations.com>
2022-06-12 22:17:28 +0530

    new commit 2

--- gitpr/gitpr/ViewController.swift
+++ gitpr/gitpr/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("new branch")
+        print("new branch 1 new commit")
     }
     
     
